bb29640579cd2495f880b82da32542dea1aac9a5
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-06-14 20:30:20 +0545

    v 0.2 only 2 single digit op oper  arith. possible

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const digits = document.querySelectorAll('.digit')
+const op = document.querySelectorAll('.op')
 const display = document.querySelector('.display-area')
 const sDelete = document.querySelector('.del')
 const clear = document.querySelector('.ac')
 const dot = document.querySelector('.num-dot')
+// const add = document.querySelector('.num-add')
+// const sub = document.querySelector('.num-sub')
+// const div = document.querySelector('.num-div')
+// const mul = document.querySelector('.num-mul')
+const eq = document.querySelector('.num-eq')
 
-const clearDisplay = () => {display.innerText = ''}
+
+
+let result = 0
+let bufferArr = []
+
+const clearDisplay = () => {
+  display.innerText = ''
+  bufferArr = [] 
+}
 
 const concatDisplay = (newData) => {
   display.innerText += newData
@@ -15,6 +29,43 @@ clear.addEventListener('click', clearDisplay)
 digits.forEach( digit => {
   digit.addEventListener('click', (e) => {
     concatDisplay(e.target.value)
+    result = e.target.value
     console.log(e.target.value)
+    console.log(bufferArr)
+    bufferArr.push(e.target.value)
   })  
 })
+
+op.forEach( operator => {
+  operator.addEventListener('click', (e) => {
+
+
+      console.log('operation of symbol')
+      bufferArr.push(`${e.target.value}`)
+      console.log(e.target.value)
+  })
+})
+
+eq.addEventListener('click', doOperation)
+
+function doOperation(){
+  console.log(bufferArr)
+  switch (bufferArr[1]) {
+    case '+':
+      result = Number(bufferArr[0]) + Number(bufferArr[2]);
+      break;
+    case '-':
+      result = Number(bufferArr[0]) - Number(bufferArr[2]);
+      break;
+    case '*':
+      result = Number(bufferArr[0]) * Number(bufferArr[2]);
+      break;
+    case '/':
+      result = Number(bufferArr[0]) / Number(bufferArr[2]);
+      break;
+    default:
+      console.log('Invalid operation symbol.');
+  }
+  console.log(result)
+display.innerText = result
+}
