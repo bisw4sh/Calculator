fe1808eef7ed6e136c73485ea4fb0ea1df52eea8
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-06-14 19:21:32 +0545

    v0.1

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const digits = document.querySelectorAll('.digit')
+const display = document.querySelector('.display-area')
+const sDelete = document.querySelector('.del')
+const clear = document.querySelector('.ac')
+const dot = document.querySelector('.num-dot')
+
+const clearDisplay = () => {display.innerText = ''}
+
+const concatDisplay = (newData) => {
+  display.innerText += newData
+}
+
+clear.addEventListener('click', clearDisplay)
 
 digits.forEach( digit => {
   digit.addEventListener('click', (e) => {
-    console.log(e)
+    concatDisplay(e.target.value)
+    console.log(e.target.value)
   })  
 })
